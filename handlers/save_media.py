45f47908e296ff90b335b99e9817f05dcddd07b8
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-08-06 20:46:16 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -55,7 +55,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             f"Just Click the link to get your files!",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Click To Shorten", url="https://t.me/share/url?url=share_link")]]
+                 [InlineKeyboardButton("Click To Shorten", url="https://t.me/share/url?url={share_link}")]]
              ),
             disable_web_page_preview=True
         )
